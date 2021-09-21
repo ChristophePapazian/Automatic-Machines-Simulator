5a0194cc09aa21b3a8481261125958e05513d2df
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-09-21 15:04:21 +0200

    Use argparse native subparser handling

--- am/__main__.py
+++ am/__main__.py
@@ -5,7 +5,30 @@ import subprocess
 from collections import defaultdict
 from sys import argv, exit, stdout
 
-def simulation(am, tape,  result_only=False, statistics=False):
+from am.am_curses import UI_Curses
+from am.am_parser import am_from_string
+from am.turing_machine import Simulation
+
+COMMANDS = {}
+
+
+def cmd(*args):
+    def inner(fct):
+        COMMANDS[fct.__name__] = args, fct
+        return fct
+
+    return inner
+
+
+@cmd(
+    ("-t", "--tape", "initial tape. First position inside angle brackets <.>", "<_>", None),
+    ("-r", "--result-only", "no simulation, result only", None, "store_true"),
+    ("-s", "--statistics", "print detailed statistics", None, "store_true")
+)
+def simulate(am, tape, result_only, statistics, **kwargs):
+    """
+    Simulate the Turing machine
+    """
     sim = Simulation(am, tape)
     if result_only:
         while sim.result is None:
@@ -34,58 +57,73 @@ def simulation(am, tape,  result_only=False, statistics=False):
     print(f"{sim.result} in {sim.steps} steps.")
 
 
-def draw(am, name, file=None):
-    '''
-    Create a pdf file from an automatic machine. 
-    Requirement : the dot program from GraphViz
-    '''
+@cmd()
+def draw(am, file=None, **kwargs):
+    """
+        Create a pdf file from an automatic machine.
+        Requirement : the dot program from GraphViz
+        """
     DOT_DATA = []
-    D={-1:"L", 0:"S", 1:"R"}
-    if file is None : file=stdout
+    D = {-1: "L", 0: "S", 1: "R"}
+    if file is None: file = stdout
     DOT_DATA.append("digraph {")
     DOT_DATA.append(" graph [mclimit = 100 rankdir = LR]")
-    # print(' edge [lblstyle="sloped"];') 
+    # print(' edge [lblstyle="sloped"];')
     DOT_DATA.append(' QI0123456789 [shape=point]')
     DOT_DATA.append(f' QI0123456789 -> {am.initial_state}')
     for s in am.transitions:
         loops = []
         T = defaultdict(list)
-        color = ["white", "0.333 0.5 1.0", "0.0 0.25 1.0", "0.1666 0.75 1.0"][int(s==am.initial_state)+2*int(s in am.end_states)]
+        color = ["white", "0.333 0.5 1.0", "0.0 0.25 1.0", "0.1666 0.75 1.0"][
+            int(s == am.initial_state) + 2 * int(s in am.end_states)]
         options = f'fillcolor="{color}"'
         DOT_DATA.append(f'"{s}" [style="filled" {options}]')
         for r in am.transitions[s]:
             w, m, ns = am.transitions[s][r]
-            T[(m,ns)].append((r,w))
-        for m,ns in T:
-            r,w = zip(*T[(m,ns)])
+            T[(m, ns)].append((r, w))
+        for m, ns in T:
+            r, w = zip(*T[(m, ns)])
             R = '<font color="blue">|</font>'.join(",".join(ro) for ro in r)
-            w = w[:1] if len(set(w))==1 else w
+            w = w[:1] if len(set(w)) == 1 else w
             W = '<font color="blue">|</font>'.join(",".join(wo) for wo in w)
-            W = "" if W==R else '<font color="red">'+W+"</font> "
+            W = "" if W == R else '<font color="red">' + W + "</font> "
             M = ",".join(D[c] for c in m)
-            if ns == s :
+            if ns == s:
                 loops.append(f'{R} {W}{M}')
-            else :
+            else:
                 DOT_DATA.append(f'"{s}" -> "{ns}" [label = <{R} {W}{",".join(D[c] for c in m)}>]')
         if loops:
             DOT_DATA.append(f'"{s}" [label = <<B>{s}</B><BR/>{"<BR/>".join(loops)}>]')
         else:
             DOT_DATA.append(f'"{s}" [label = <<B>{s}</B>>]')
     for s in am.end_states:
-        color = ["white", "0.333 0.5 1.0", "0.0 0.25 1.0", "0.1666 0.75 1.0"][int(s==am.initial_state)+2*int(s in am.end_states)]
+        color = ["white", "0.333 0.5 1.0", "0.0 0.25 1.0", "0.1666 0.75 1.0"][
+            int(s == am.initial_state) + 2 * int(s in am.end_states)]
         options = f'fillcolor="{color}"'
         DOT_DATA.append(f'"{s}" [style="filled" {options} shape=doubleoctagon];')
     DOT_DATA.append("}")
-    dot_proc = subprocess.Popen(['dot', '-Tpdf', '-o'+name+'.pdf'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    dot_proc = subprocess.Popen(['dot', '-Tpdf', '-o' + am.name + '.pdf'], stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
     outs, errs = dot_proc.communicate(input='\n'.join(DOT_DATA).encode('UTF8'))
     print(*(l for l in outs), end='')
     print(*(l for l in errs), end='')
 
+
 def main():
     main_parser = argparse.ArgumentParser()
-    main_parser.add_argument("command", help="simulate|draw")
 
-    args = main_parser.parse_args(argv[1:2])
+    subparsers = main_parser.add_subparsers(dest="command", required=True)
+
+    for name, (args, fct) in COMMANDS.items():
+        parser = subparsers.add_parser(name, aliases=[name[:3]], help=fct.__doc__)
+        parser.set_defaults(func=fct)
+        parser.add_argument("filename", help="input filename containing machine description")
+        parser.add_argument("-n", "--name", help="name of the machine used", default=None)
+        for short, long, help, default, action in args:
+            parser.add_argument(short, long, help=help, default=default, action=action)
+
+    args = main_parser.parse_args(argv[1:])
 
     def get_name(filename, name):
         with open(filename, 'r') as input:
@@ -98,35 +136,18 @@ def main():
                     for m in lm:
                         print(m.name)
                     exit(1)
-            elif len(lm)==1:
+            elif len(lm) == 1:
                 return lm[0]
             else:
                 print(f"{len(lm)} machines found. Names available are (use -n name):")
                 for m in lm:
-                    print(f"{m.name:12} with {m.nb_tapes} tape{'s,' if m.nb_tapes>1 else ', '} {len(m.transitions):3} states and {len(m.end_states):2} final state{'s.' if len(m.end_states)>1 else '.'}")
+                    print(
+                        f"{m.name:12} with {m.nb_tapes} tape{'s,' if m.nb_tapes > 1 else ', '} {len(m.transitions):3} states and {len(m.end_states):2} final state{'s.' if len(m.end_states) > 1 else '.'}")
                 exit(1)
 
-    ### SIMULATION
-    if args.command[:3] == "sim":
-        parser = argparse.ArgumentParser()
-        parser.add_argument("filename", help="input filename containing machine description")
-        parser.add_argument("-t", "--tape", help="initial tape. First position inside angle brackets <.>", default="<_>")
-        parser.add_argument("-n", "--name", help="name of the machine used", default=None)
-        parser.add_argument("-r", "--result-only", help="no simulation, result only", action="store_true")
-        parser.add_argument("-s", "--statistics", help="print detailed statistics", action="store_true")
-        args = parser.parse_args(argv[2:])
-        m = get_name(args.filename, args.name)
-        simulation(m, args.tape, result_only=args.result_only, statistics=args.statistics)
-    ### DRAW
-    elif args.command[:3] == "dra":
-        parser = argparse.ArgumentParser()
-        parser.add_argument("filename", help="input filename containing machine description")
-        parser.add_argument("-n", "--name", help="name of the machine used", default=None)
-        args = parser.parse_args(argv[2:])
-        m = get_name(args.filename, args.name)
-        draw(m, args.name)
-    else:
-        print("unknown command {args.command}.")
+    m = get_name(args.filename, args.name)
+    args.func(m, args=args, **vars(args))
+
 
 if __name__ == '__main__':
     main()
